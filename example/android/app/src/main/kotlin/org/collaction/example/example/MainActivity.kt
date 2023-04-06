630a2862b2cba8397e612bd07dccce233483a805
Isaac Obella <wizlif@users.noreply.github.com>
2023-04-06 18:13:00 +0300

    core: create example app for wiidget demo and preview
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

---
+++ example/android/app/src/main/kotlin/org/collaction/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.collaction.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:common/theme.dart';
+import 'package:example/demo_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final theme = ValueNotifier(ThemeMode.system);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: theme,
+        builder: (context, appTheme, _) {
+          return MaterialApp(
+            title: 'CollAction Components Demo',
+            theme: lightTheme(),
+            darkTheme: darkTheme(),
+            themeMode: appTheme,
+            home: const DemoPage(),
+          );
+        });
+  }
+}
