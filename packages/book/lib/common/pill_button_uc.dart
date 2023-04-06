be0e5274bd33b0c236bd5e88ca1764013c09ee3d
Isaac Obella <wizlif@users.noreply.github.com>
2023-04-07 01:59:19 +0300

    book(pill_button): Add figma description
    Signed-off-by: Isaac Obella <wizlif@users.noreply.github.com>

--- packages/book/lib/common/pill_button_uc.dart
+++ packages/book/lib/common/pill_button_uc.dart
@@ -3,16 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart' show Knobs;
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-@WidgetbookUseCase(name: 'PillButton', type: PillButton)
+@WidgetbookUseCase(
+  name: 'PillButton',
+  type: PillButton,
+  designLink: "https://www.figma.com/file/dYFW0QF7Fg10dpSIxA3wQb/"
+      "CollAction-App?node-id=70-217&t=QxAbOu88TrcMOCbF-4",
+)
 Widget buildPillButton(BuildContext context) => Center(
-      child: PillButton(
-        text: context.knobs
-            .text(label: 'Button text', initialValue: 'Upload File'),
-        onTap: context.knobs.boolean(label: 'Can be clicked') ? () {} : null,
-        isLoading: context.knobs.boolean(label: 'Is loading'),
-        isEnabled: context.knobs.boolean(label: 'Is enabled'),
-        leading: context.knobs.boolean(label: 'Has leading icon')
-            ? Icon(Icons.upload)
-            : null,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: PillButton(
+          text: context.knobs
+              .text(label: 'Button text', initialValue: 'Upload File'),
+          onTap: context.knobs.boolean(label: 'Has onTap') ? () {} : null,
+          isLoading: context.knobs.boolean(label: 'Is loading'),
+          isEnabled: context.knobs.boolean(label: 'Is enabled'),
+          leading: context.knobs.boolean(label: 'Has leading icon')
+              ? Icon(Icons.upload)
+              : null,
+        ),
       ),
     );
